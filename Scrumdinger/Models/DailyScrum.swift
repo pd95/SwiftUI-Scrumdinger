0c0c49e4f5b6fbb5b6329f6951ff17ece1717d3f
Philipp <pd95@users.noreply.github.com>
2020-12-30 18:46:11 +0100

    After "Creating the Edit View"

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -23,7 +23,6 @@ struct DailyScrum: Identifiable {
     }
 }
 
-
 extension DailyScrum {
     static var data: [DailyScrum] {
         [
@@ -31,5 +30,18 @@ extension DailyScrum {
             DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, color: Color("App Dev")),
             DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 1, color: Color("Web Dev"))
         ]
+    }
+}
+
+extension DailyScrum {
+    struct Data {
+        var title: String = ""
+        var attendees: [String] = []
+        var lengthInMinutes: Double = 5.0
+        var color: Color = .random
+    }
+
+    var data: Data {
+        return Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), color: color)
     }
 }
